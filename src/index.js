a7f05f162f9cb93bc90089faaf0d548c09eb8f97
gitdevin99 <sanjananb1@gmail.com>
2025-02-06 23:34:38 +0400

    Fix duplicate RedisStore declaration

--- src/index.js
+++ src/index.js
@@ -26,9 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Initialize RedisStore
-const RedisStore = require('connect-redis').default;
-
 // Session middleware
 app.use(session({
     store: new RedisStore({
